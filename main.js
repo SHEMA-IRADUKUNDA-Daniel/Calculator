aa58753fb42f4a32ec416ec3482e0c0e6c2566b2
Daniel SHEMA IRADUKUNDA <94281249+SHEMA-IRADUKUNDA-Daniel@users.noreply.github.com>
2023-08-16 17:20:19 +0200

    making the app function

--- main.js
+++ main.js
@@ -3,32 +3,34 @@ const operators = document.querySelectorAll(".btnYellow");
 const screen = document.querySelector(".screen");
 const clear = document.querySelector(".btnClear");
 const equal = document.querySelector(".btnEqual");
+const decimal = document.querySelector(".decimal");
 
 function add(num1, num2) {
     const sum = Number.parseFloat(num1) + Number.parseFloat(num2);
     return sum;
-}
+};
 function subtract(num1, num2) {
     const sum = num1 - num2;
     return sum;
-}
+};
 function multiply(num1, num2) {
     const sum = num1 * num2;
     return sum;
-}
+};
 function divide(num1, num2) {
     const sum = num1 / num2;
     return sum;
-}
+};
+
 
 let num1 ="";
 let num2 ="";
 let operator ="";
 
-
-
 function operate() {
     let result;
+
+
     num2 = screen.value;
 
     if (operator === "+") {
@@ -47,8 +49,11 @@ function operate() {
         screen.value = "Input something!";
         screen.style.fontSize = "30px";
         screen.style.color = "red";
+        screen.style.paddingRight = "20px"
         
     };
+
+    
 };
     operators.forEach(function (button) {
     button.addEventListener("click", function (e) {
@@ -61,14 +66,37 @@ function operate() {
     button.addEventListener("click", function (e) {
         let value = e.target.dataset.num;
 
+
+        if(screen.value.includes(".") && value === "."){
+            return;
+        }
+
+        if(operator === "/" && num2 == 0){
+            screen.value = "cannot divide by zero";
+            screen.style.fontSize = "26px";
+            screen.style.color = "red";
+            return;
+        }        
+                
+        if (num1 && operator && num2) {
+            const result = calculateResult(num1, operator, num2);
+            screen.value = result.toString();
+            num1 = result.toString();
+            num2 = "";
+            operator = "";
+        }
+
+
         if (operator && !num2) {
         screen.value = "";
         };
 
+        screen.style.paddingRight = "20px";
+        
         screen.value += value;
         screen.style.color = "white";
     });
-    });
+});
 
     equal.addEventListener("click", operate);
 
